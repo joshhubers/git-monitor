23578629d0bdc8aff33e305788c96870cd3f9268
Josh Hubers <jhubers@oaisd.org>
2018-04-23 12:40:18 -0400

    add github integration. Fixed row formatting

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,18 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   gitInfo: null,
+  githubSession: service(),
+  store: service(),
+  foo: null,
 
   init() {
+    this.get('githubSession').set('githubAccessToken', '');
+
+    this.get('store').query('github-branch', { repo: 'OAISD/inqwizit-uae-web-app' } ).then((branches) => { debugger; });
+      
+
     this.set('gitInfo', [
       { name: 'test-branch-error', status: 'error' },
       { name: 'test-branch-failure', status: 'failure' },
