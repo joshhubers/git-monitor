e855a437d94d8441bb330537900f4e45de50f3b9
Josh Hubers <jhubers@oaisd.org>
2018-05-28 16:40:41 -0400

    added polling

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,19 +1,32 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default Controller.extend({
   githubSession: service(),
   store: service(),
-  foo: null,
   gitInfo: null,
   monitorRepo: 'my-repo',
   token: '',
 
   init() {
     this.set('gitInfo', []);
-    this.get('githubSession').set('githubAccessToken', this.get('token'));
+    get(this, 'githubSession').set('githubAccessToken', this.get('token'));
 
-    this.get('store').query('github-branch', { repo: this.get('monitorRepo') } ).then(branches => { 
+    this.fetchAndBuildBranches();
+    this.poll();
+  },
+
+  poll() {
+    Ember.run.later(this, 
+      function() { 
+        this.fetchAndBuildBranches();
+        this.poll();
+      }, 10000);
+  },
+
+  fetchAndBuildBranches() {
+    get(this, 'store').query('github-branch', { repo: this.get('monitorRepo') }, { reload : true } ).then(branches => { 
       this.buildBranches(branches);
     });
   },
@@ -22,9 +35,16 @@ export default Controller.extend({
     branches.forEach(b => {
       const branchSha = b.commit.sha;
 
-      this.get('store').queryRecord('github-state', { repo: this.get('monitorRepo'), ref: branchSha}).then(gitState => {
+      get(this, 'store').queryRecord('github-state', { repo: this.get('monitorRepo'), ref: branchSha}).then(gitState => {
         const branchState = { name: b.name, status: gitState.state };
-        this.get('gitInfo').pushObject(branchState);
+
+        const existing = get(this, 'gitInfo').find(gi => gi.name === branchState.name);
+        if(existing) {
+          this.set('existing', 'status', get(branchState, 'status'));
+        } else {
+          get(this, 'gitInfo').pushObject(branchState);
+        }
+
       });
     });
   },
